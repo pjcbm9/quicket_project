e0857a4c311cd97f680759355596c57a47062275
pjcbm9 <pjcbm9@mail.umkc.edu>
2014-12-05 22:31:54 -0600

    updated android studio

---
+++ app/src/main/java/pjcbm9/quicket/TicketStatusPackage/SearchModel.java
@@ -0,0 +1,7 @@
+package pjcbm9.quicket.TicketStatusPackage;
+
+/**
+ * Created by Jason Crow on 12/5/2014.
+ */
+public class SearchModel {
+}
---
+++ app/src/main/java/pjcbm9/quicket/TicketStatusPackage/StatusModel.java
@@ -0,0 +1,7 @@
+package pjcbm9.quicket.TicketStatusPackage;
+
+/**
+ * Created by Jason Crow on 12/5/2014.
+ */
+abstract class StatusModel {
+}
